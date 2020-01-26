3867903fdb8eb7f566cd32ba63c075c73310a508
Brandon McFarlin <brandonmcfarlin1016@gmail.com>
2020-01-25 20:07:22 -0500

    reauthenticate google access token
    After about an hour the access token will expire. This reauthenticates and gets a new access token every hour.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let Accessory, hap, UUIDGen;
-const Nest = require('./lib/nest').NestAPI;
+const Nest = require('./lib/nest.js').NestAPI;
 const NestConnection = require('./lib/nest-connection.js');
 const Promise = require('bluebird');
 
@@ -14,26 +14,26 @@ module.exports = (homebridge) => {
 }
 
 const setupConnection = function(config, log) {
-    return new Promise(function (resolve, reject) {
-        if (!config.access_token && !config.googleAuth && (!config.email || !config.password)) {
-            reject('You did not specify your Nest account credentials {\'email\',\'password\'}, or an access_token, or googleAuth, in config.json');
-            return;
-        }
+  return new Promise(function (resolve, reject) {
+    if (!config.access_token && !config.googleAuth && (!config.email || !config.password)) {
+      reject('You did not specify your Nest account credentials {\'email\',\'password\'}, or an access_token, or googleAuth, in config.json');
+        return;
+    }
 
-        if (config.googleAuth && (!config.googleAuth.issueToken || !config.googleAuth.cookies || !config.googleAuth.apiKey)) {
-            reject('When using googleAuth, you must provide issueToken, cookies and apiKey in config.json. Please see README.md for instructions');
-            return;
-        }
+    if (config.googleAuth && (!config.googleAuth.issueToken || !config.googleAuth.cookies || !config.googleAuth.apiKey)) {
+      reject('When using googleAuth, you must provide issueToken, cookies and apiKey in config.json. Please see README.md for instructions');
+      return;
+    }
 
-        const conn = new NestConnection(config, log);
-        conn.auth().then(connected => {
-            if (connected) {
-                resolve(conn);
-            } else {
-                reject('Unable to connect to Nest service.');
-            }
-        });
+    const conn = new NestConnection(config, log);
+    conn.auth().then(connected => {
+      if (connected) {
+        resolve(conn);
+      } else {
+        reject('Unable to connect to Nest service.');
+      }
     });
+  });
 };
 
 class NestCamPlatform {
@@ -55,9 +55,13 @@ class NestCamPlatform {
     // Won't be invoked
   }
 
+  /**
+   * Add fetched cameras from nest to Homebridge
+   * @param accessToken The google access token
+   */
   addCameras(accessToken) {
     let self = this;
-    self.nestAPI = new Nest(accessToken, self.log);
+    self.nestAPI = new Nest(accessToken, self.config, self.log);
     self.nestAPI.on('cameras', (cameras) => {
       let configuredAccessories = [];
       cameras.forEach((camera) => {
