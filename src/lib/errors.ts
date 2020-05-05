5e23cc963f4301452dd684b438b395ee2d9c93b9
Brandon McFarlin <brandonmcfarlin1016@gmail.com>
2020-05-05 14:02:00 -0400

    fix codacy errors

--- src/lib/errors.ts
+++ src/lib/errors.ts
@@ -1,9 +1,9 @@
 export class APIError extends Error {
   public code: number;
-  
+
   constructor(message: string, code: number) {
     super(message); // (1)
-    this.name = "APIError"; // (different names for different built-in error classes)
+    this.name = 'APIError'; // (different names for different built-in error classes)
     this.code = code;
   }
 }
