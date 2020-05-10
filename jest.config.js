cbf98d16aabc4cfcda70234aac868fe39d63b942
Brandon McFarlin <brandonmcfarlin1016@gmail.com>
2020-05-10 15:49:01 -0400

    reorganized testing

--- jest.config.js
+++ jest.config.js
@@ -139,7 +139,7 @@ module.exports = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "__tests__/*.ts",
+    "**/test/*.spec.ts",
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
--- test/NestEndpoints.spec.ts
+++ test/NestEndpoints.spec.ts
@@ -2,7 +2,7 @@ import { NestEndpoints } from '../src/nest-endpoints';
 
 test('Nest Endpoints', () => {
     let endpoints = new NestEndpoints(false);
-    endpoints.sendRequest(undefined, 'https://store.nest.com/', `/mt-api/v1/current`, 'GET').then(data => {
+    endpoints.sendRequest(undefined, 'https://store.nest.com', `/mt-api/v1/current`, 'GET').then(data => {
         expect(data.isLoggedIn).toBe(false);
     });
 });
