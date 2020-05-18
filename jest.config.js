bfaee08d401cab4fbb70a7f0a495cae849156761
Brandon McFarlin <brandonmcfarlin1016@gmail.com>
2020-05-18 16:49:04 -0400

    add display name

--- jest.config.js
+++ jest.config.js
@@ -129,7 +129,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  testEnvironment: 'node',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -138,9 +138,7 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: [
-    "**/test/*.spec.ts",
-  ],
+  testMatch: ['**/test/*.spec.ts'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
