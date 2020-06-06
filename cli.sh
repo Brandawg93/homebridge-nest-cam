deb1a36579a6753c222a0755a1863624e77228d9
Brandon McFarlin <brandonmcfarlin1016@gmail.com>
2020-06-06 15:46:51 -0400

    passthrough cl args

--- cli.sh
+++ cli.sh
@@ -2,19 +2,11 @@
 
 if [ "$1" == "login" ] || [ "$1" == "-l" ]; then
     if [[ $(npm list puppeteer) = *puppeteer* ]]; then
-        if [ "$2" == "-h" ]; then
-            node dist/login.js -h
-        else
-            node dist/login.js
-        fi
+        node dist/login.js "$@"
     else
         read -p "The latest version of Chromium browser will be downloaded. Would you like to continue? (y/N): " convar
         if [ "$convar" == "y" ] || [ "$convar" == "Y" ]; then
-            if [ "$2" == "-h" ]; then
-                npm install puppeteer puppeteer-extra puppeteer-extra-plugin-stealth@2.4.5 && node dist/login.js -h
-            else
-                npm install puppeteer puppeteer-extra puppeteer-extra-plugin-stealth@2.4.5 && node dist/login.js
-            fi
+            npm install puppeteer puppeteer-extra puppeteer-extra-plugin-stealth@2.4.5 && node dist/login.js "$@"
         else
             echo "Exiting..."
         fi
