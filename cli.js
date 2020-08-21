10761509cebcf8ebb52662512be983bc6ae04fb3
Brandon McFarlin <brandonmcfarlin1016@gmail.com>
2020-08-21 09:35:55 -0400

    removed clean command

--- cli.js
+++ cli.js
@@ -2,16 +2,9 @@
 
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const child_process = require('child_process');
-
 if (process.argv.includes('login')) {
   require('./dist/login');
-} else if (process.argv.includes('clean')) {
-  child_process.execSync('npm prune --production', {
-      stdio: 'inherit',
-      cwd: __dirname,
-  });
 } else {
-  console.log('Invalid Command');
+  console.error('Invalid Command');
   process.exit(1);
 }
