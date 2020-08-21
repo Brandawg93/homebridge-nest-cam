88a3af2736f017017ede2fc90ee3d06d9c8d3e8c
oznu <dev@oz.nu>
2020-08-21 22:21:36 +1000

    make cli cross-platform compatible, fix post install script

--- cli.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "login" ] || [ "$1" == "-l" ]; then
-    node dist/login.js "$@"
-elif [ "$1" == "clean" ] || [ "$1" == "-c" ]; then
-    npm prune --production
-else
-    echo "Invalid command."
-fi
