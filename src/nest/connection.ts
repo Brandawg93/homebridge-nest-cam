29c2fd0f6e324595d12837b5dfe1a236494a82cd
Bo Li <lubao515@gmail.com>
2020-11-06 18:57:40 -0800

    Add nest account support

--- src/nest/connection.ts
+++ src/nest/connection.ts
@@ -52,75 +52,82 @@ export class Connection {
 
   /**
    * Attempt to authenticate Nest via Google account
+   * If access_token exist, use directly
    */
   async auth(): Promise<boolean> {
-    let req: AxiosRequestConfig;
+    if (this.config.googleAuth){
+        let req: AxiosRequestConfig;
 
-    if (!this.config.googleAuth || !this.config.googleAuth.issueToken || !this.config.googleAuth.cookies) {
-      this.log.error('The plugin configuration is missing values.');
-      return false;
-    }
+        if (!this.config.googleAuth || !this.config.googleAuth.issueToken || !this.config.googleAuth.cookies) {
+          this.log.error('The plugin configuration is missing values.');
+          return false;
+        }
 
-    //Only doing google auth from now on
-    const issueToken = this.config.googleAuth.issueToken.replace('Request URL: ', '');
-    const cookies = this.config.googleAuth.cookies.replace('cookie: ', '');
-    const apiKey =
-      this.config.googleAuth.apiKey?.replace('x-goog-api-key: ', '') ||
-      (this.config.fieldTest ? 'AIzaSyB0WNyJX2EQQujlknzTDD9jz7iVHK5Jn-U' : 'AIzaSyAdkSIMNc51XGNEAYWasX9UOWkS5P6sZE4');
+        //Only doing google auth from now on
+        const issueToken = this.config.googleAuth.issueToken.replace('Request URL: ', '');
+        const cookies = this.config.googleAuth.cookies.replace('cookie: ', '');
+        const apiKey =
+          this.config.googleAuth.apiKey?.replace('x-goog-api-key: ', '') ||
+          (this.config.fieldTest ? 'AIzaSyB0WNyJX2EQQujlknzTDD9jz7iVHK5Jn-U' : 'AIzaSyAdkSIMNc51XGNEAYWasX9UOWkS5P6sZE4');
 
-    this.log.debug('Authenticating via Google.');
-    let result;
-    try {
-      req = {
-        method: 'GET',
-        timeout: API_TIMEOUT_SECONDS * 1000,
-        url: issueToken,
-        headers: {
-          'Sec-Fetch-Mode': 'cors',
-          'User-Agent': NestEndpoints.USER_AGENT_STRING,
-          'X-Requested-With': 'XmlHttpRequest',
-          Referer: 'https://accounts.google.com/o/oauth2/iframe',
-          cookie: cookies,
-        },
-      };
-      result = (await axios(req)).data;
-      const googleAccessToken = result.access_token;
-      if (result.error) {
-        this.log.error(
-          'Google authentication was unsuccessful. Make sure you did not log out of your Google account after getting your googleAuth parameters.',
-        );
-        throw result;
+        this.log.debug('Authenticating via Google.');
+        let result;
+        try {
+          req = {
+            method: 'GET',
+            timeout: API_TIMEOUT_SECONDS * 1000,
+            url: issueToken,
+            headers: {
+              'Sec-Fetch-Mode': 'cors',
+              'User-Agent': NestEndpoints.USER_AGENT_STRING,
+              'X-Requested-With': 'XmlHttpRequest',
+              Referer: 'https://accounts.google.com/o/oauth2/iframe',
+              cookie: cookies,
+            },
+          };
+          result = (await axios(req)).data;
+          const googleAccessToken = result.access_token;
+          if (result.error) {
+            this.log.error(
+              'Google authentication was unsuccessful. Make sure you did not log out of your Google account after getting your googleAuth parameters.',
+            );
+            throw result;
+          }
+          req = {
+            method: 'POST',
+            timeout: API_TIMEOUT_SECONDS * 1000,
+            url: 'https://nestauthproxyservice-pa.googleapis.com/v1/issue_jwt',
+            data: {
+              embed_google_oauth_access_token: true,
+              expire_after: '3600s', //expire the access token in 1 hour
+              google_oauth_access_token: googleAccessToken,
+              policy_id: 'authproxy-oauth-policy',
+            },
+            headers: {
+              Authorization: 'Bearer ' + googleAccessToken,
+              'User-Agent': NestEndpoints.USER_AGENT_STRING,
+              'x-goog-api-key': apiKey,
+              Referer: this.endpoints.NEST_API_HOSTNAME,
+            },
+          };
+          result = (await axios(req)).data;
+          this.config.access_token = result.jwt;
+          return true;
+        } catch (error) {
+          error.status = error.response && error.response.status;
+          this.log.error('Access token acquisition via googleAuth failed (code ' + (error.status || error.code) + ').');
+          if (['ECONNREFUSED', 'ESOCKETTIMEDOUT', 'ECONNABORTED', 'ENOTFOUND', 'ENETUNREACH'].includes(error.code)) {
+            this.log.error('Retrying in ' + API_AUTH_FAIL_RETRY_DELAY_SECONDS + ' second(s).');
+            await delay(API_AUTH_FAIL_RETRY_DELAY_SECONDS * 1000);
+            return await this.auth();
+          } else {
+            return false;
+          }
+        }
+      elif (!this.config.access_token){
+          this.log.error('The plugin configuration is missing values.');
+          return false;
       }
-      req = {
-        method: 'POST',
-        timeout: API_TIMEOUT_SECONDS * 1000,
-        url: 'https://nestauthproxyservice-pa.googleapis.com/v1/issue_jwt',
-        data: {
-          embed_google_oauth_access_token: true,
-          expire_after: '3600s', //expire the access token in 1 hour
-          google_oauth_access_token: googleAccessToken,
-          policy_id: 'authproxy-oauth-policy',
-        },
-        headers: {
-          Authorization: 'Bearer ' + googleAccessToken,
-          'User-Agent': NestEndpoints.USER_AGENT_STRING,
-          'x-goog-api-key': apiKey,
-          Referer: this.endpoints.NEST_API_HOSTNAME,
-        },
-      };
-      result = (await axios(req)).data;
-      this.config.access_token = result.jwt;
-      return true;
-    } catch (error) {
-      error.status = error.response && error.response.status;
-      this.log.error('Access token acquisition via googleAuth failed (code ' + (error.status || error.code) + ').');
-      if (['ECONNREFUSED', 'ESOCKETTIMEDOUT', 'ECONNABORTED', 'ENOTFOUND', 'ENETUNREACH'].includes(error.code)) {
-        this.log.error('Retrying in ' + API_AUTH_FAIL_RETRY_DELAY_SECONDS + ' second(s).');
-        await delay(API_AUTH_FAIL_RETRY_DELAY_SECONDS * 1000);
-        return await this.auth();
-      } else {
-        return false;
-      }
-    }
+      return true
   }
 }
