ec6ea0527dee5736d469df9fd51a3d1ad5cbf71c
Brandon McFarlin <brandonmcfarlin1016@gmail.com>
2020-11-28 16:34:06 -0500

    config based on capabilities

--- src/homebridge-ui/public/app/app.component.ts
+++ src/homebridge-ui/public/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CameraInfo } from '../../../nest/models/camera';
 
 @Component({
   selector: 'app-root',
@@ -25,16 +26,14 @@ export class AppComponent implements OnInit {
       });
       if (this.authenticated) {
         // create the form
+        const self = this;
         let schema = (await this.homebridge.getPluginConfigSchema()).schema;
         if (schema) {
           schema = await this.modifySchema(schema);
-          const form = this.homebridge.createForm(
-            { schema: schema },
-            { googleAuth: { issueToken: issueToken, cookies: cookies } },
-          );
+          const form = this.homebridge.createForm({ schema: schema }, config);
           // watch for change events
-          form.onChange((change) => {
-            console.log(change);
+          form.onChange(async (change) => {
+            await self.homebridge.updatePluginConfig([change]);
           });
 
           // stop listening to change events and hide the form
@@ -48,22 +47,32 @@ export class AppComponent implements OnInit {
   }
 
   async modifySchema(schema: Record<string, any>): Promise<Record<string, any>> {
+    const cameras = (await this.homebridge.request('/cameras')) as Array<CameraInfo>;
     const structures = await this.homebridge.request('/structures');
 
-    if (
-      schema &&
-      schema.options &&
-      schema.options.properties &&
-      schema.options.properties.structures &&
-      schema.options.properties.structures.items
-    ) {
-      if (structures.length > 1) {
-        schema.options.properties.structures.items.oneOf = structures;
-      } else {
-        delete schema.options.properties.structures;
+    const hasDoorbell = cameras.some((c) => c.capabilities.includes('indoor_chime'));
+    const hasMotion = cameras.some((c) => c.capabilities.includes('detectors.on_camera'));
+
+    if (schema && schema.options && schema.options.properties) {
+      if (!hasDoorbell) {
+        delete schema.options.properties.doorbellAlerts;
+        delete schema.options.properties.doorbellSwitch;
+      }
+      if (!hasMotion) {
+        delete schema.options.properties.alertCheckRate;
+        delete schema.options.properties.alertCooldownRate;
+        delete schema.options.properties.alertTypes;
+        delete schema.options.properties.importantOnly;
+        delete schema.options.properties.motionDetection;
+      }
+      if (schema.options.properties.structures && schema.options.properties.structures.items) {
+        if (structures.length > 1) {
+          schema.options.properties.structures.items.oneOf = structures;
+        } else {
+          delete schema.options.properties.structures;
+        }
       }
     }
-
     return schema;
   }
 
--- src/homebridge-ui/server.ts
+++ src/homebridge-ui/server.ts
@@ -1,6 +1,7 @@
 import { HomebridgePluginUiServer } from '@homebridge/plugin-ui-utils';
 import { auth, getCameras } from '../nest/connection';
 import { NestConfig } from '../nest/models/config';
+import { CameraInfo } from '../nest/models/camera';
 
 interface Structure {
   name: string;
@@ -16,11 +17,23 @@ class UiServer extends HomebridgePluginUiServer {
 
     this.onRequest('/auth', this.handleAuthRequest.bind(this));
     this.onRequest('/structures', this.handleStructureRequest.bind(this));
+    this.onRequest('/cameras', this.handleCamerasRequest.bind(this));
 
     this.ready();
     // setTimeout(() => {
     //   this.pushEvent('auth-error', { message: 'Something went wrong.' });
     // }, 2000);
+  }
+
+  private generateConfig(): NestConfig | undefined {
+    if (this.issueToken && this.accessToken) {
+      const config: NestConfig = {
+        platform: 'Nest-cam',
+        fieldTest: this.issueToken?.endsWith('https%3A%2F%2Fhome.ft.nest.com'),
+        access_token: this.accessToken,
+      };
+      return config;
+    }
   }
 
   async handleAuthRequest(payload: any): Promise<boolean> {
@@ -33,25 +46,31 @@ class UiServer extends HomebridgePluginUiServer {
     }
   }
 
-  async handleStructureRequest(): Promise<Array<Structure>> {
-    const config: NestConfig = {
-      platform: 'Nest-cam',
-      fieldTest: this.issueToken?.endsWith('https%3A%2F%2Fhome.ft.nest.com'),
-      access_token: this.accessToken,
-    };
+  async handleStructureRequest(): Promise<Array<Structure> | undefined> {
+    const config = this.generateConfig();
 
-    const structures: Array<Structure> = [];
-    const cameras = await getCameras(config);
-    cameras.forEach((cameraInfo) => {
-      const exists = structures.find((x) => x.id === cameraInfo.nest_structure_id.replace('structure.', ''));
-      if (!exists) {
-        structures.push({
-          name: cameraInfo.nest_structure_name,
-          id: cameraInfo.nest_structure_id.replace('structure.', ''),
-        });
-      }
-    });
-    return structures;
+    if (config) {
+      const structures: Array<Structure> = [];
+      const cameras = await getCameras(config);
+      cameras.forEach((cameraInfo) => {
+        const exists = structures.find((x) => x.id === cameraInfo.nest_structure_id.replace('structure.', ''));
+        if (!exists) {
+          structures.push({
+            name: cameraInfo.nest_structure_name,
+            id: cameraInfo.nest_structure_id.replace('structure.', ''),
+          });
+        }
+      });
+      return structures;
+    }
+  }
+
+  async handleCamerasRequest(): Promise<Array<CameraInfo> | undefined> {
+    const config = this.generateConfig();
+    if (config) {
+      const cameras = await getCameras(config);
+      return cameras;
+    }
   }
 }
 
