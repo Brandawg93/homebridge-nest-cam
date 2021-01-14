999e53f3263d5dd9ca84fbd616771eb124effcb4
Brandon McFarlin <6525520+Brandawg93@users.noreply.github.com>
2021-01-13 19:15:15 -0500

    update instructions

--- src/cli.ts
+++ src/cli.ts
@@ -30,7 +30,7 @@ const prompt = (query: string): Promise<string> =>
     console.log(`1. Open the url below in a browser to continue:\n\n${url}\n`);
     console.log('2. Open Developer Tools (View/Developer/Developer Tools).');
     console.log("3. Click on 'Network' tab. Make sure 'Preserve Log' is checked.");
-    console.log("4. In the 'Filter' box, enter 'nest-account'.");
+    console.log("4. In the 'Filter' box, enter 'nest-account' and select 'Doc' for the filter type.");
     console.log('5. Login to your Google account.');
     console.log("6. Click on the call beginning with 'nest-account&authuser=...'");
     let requestUrl = await prompt('7. Copy the entire Request Url here: ');
